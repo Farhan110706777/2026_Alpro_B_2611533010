85d57179aabd22fac4d03cae0f6cd5217f73d7e5
Farhan110706777 <hfar0230@gmail.com>
2026-09-08 21:56:38 +0700

    Add files via upload

---
+++ program1_2611533010.py
@@ -0,0 +1,13 @@
+# program pertama
+"""
+komentar
+dua baris atau lebih
+pertemuan 1 praktikum alpro
+"""
+print("hello world")
+print()
+print("program ini menghasilkan")
+print("Empat baris Output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini "+" dibuat oleh a\n"+"Muhammad Farhan Amri dan 2611533010")
+
