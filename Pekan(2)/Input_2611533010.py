12059af100e32f61d45d76354b58e8b94ca3aa1a
Farhan110706777 <hfar0230@gmail.com>
2026-09-12 23:37:38 +0700

    Add files via upload

---
+++ Pekan(2)/Input_2611533010.py
@@ -0,0 +1,10 @@
+# Buat file dengan nama Input_NIM.py
+# Buat program untuk menghitung luas persegi panjang
+# Nama variabel ditambah 4 digit nim terakhir contoh: panjang_1234
+# Program ini menggunakan fungsi input () untuk meminta pengguna
+# Memasukkan nilai panjang dan lebar persegi panjang
+# Nilai yang dimasukkan akan dikonversi menjadi tipe data integer
+panjang_3010 = input ("Masukkan nilai panjang")
+lebar_3010 = input ("Masukkan nilai lebar")
+luas_3010 = int(panjang_3010) * int(lebar_3010)
+print("luas =", luas_3010)
